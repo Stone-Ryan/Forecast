a162606cdae85f022c985ec85c640eb3780b38de
SDRyan9 <stonedouglasryan@gmail.com>
2018-01-01 22:43:42 -0800

    added axios package, filled out fetchWeather action to get Search bar fully functioning

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
 import { API_KEY } from '../config/keys';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const fetchWeather = city => {
+  const url = `${ROOT_URL}&q=${city},us`
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+};
